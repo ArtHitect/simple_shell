4bc3ba72df0bc9837e6be711a98e2524b9f90a08
ArtHitect <Oratilediale11122004@gmail.com>
2023-12-15 10:36:08 -0800

    simple_shell

--- shell.h
+++ shell.h
@@ -9,25 +9,25 @@ int main(void);
  *
  * @c: The character to check.
  * @delim: The delimiter string.
- * 
+ *
  * Return: 1 if true, 0 if false.
  */
 int is_delim(char c, const char *delim);
 
 /**
  * _isalpha - Checks for alphabetic character.
- * 
+ *
  * @c: The character to check.
- * 
+ *
  * Return: 1 if c is alphabetic, 0 otherwise.
  */
 int _isalpha(int c);
 
 /**
  * _atoi - Converts a string to an integer.
- * 
+ *
  * @s: The string to be converted.
- * 
+ *
  * Return: 0 if no numbers in string, converted number otherwise.
  */
 int _atoi(char *s);
